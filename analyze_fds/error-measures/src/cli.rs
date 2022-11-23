47fe2fd02f92ba2016bcfd55b5447755aa42064c
Niklas Mohrin <dev@niklasmohrin.de>
2022-11-23 21:25:11 +0100

    add mode to output approximation metrics (correctness, completeness)

--- analyze_fds/error-measures/src/cli.rs
+++ analyze_fds/error-measures/src/cli.rs
@@ -1,18 +1,35 @@
 use std::path::PathBuf;
 
-use clap::{Parser, ValueEnum};
+use clap::{Parser, Subcommand};
 
 #[derive(Debug, Parser)]
 #[command(author, version, about, long_about = None)]
 pub struct Cli {
-    #[arg(value_enum)]
-    pub command: Command,
     pub relation_path: PathBuf,
-    pub fd_path: Vec<PathBuf>,
+    #[command(subcommand)]
+    pub command: Command,
 }
 
-#[derive(Debug, Clone, Copy, ValueEnum)]
+#[derive(Debug, Clone, Subcommand)]
 pub enum Command {
-    ErrorSummary,
-    OutputErrors,
+    ErrorSummary {
+        fds: Vec<PathBuf>,
+    },
+    OutputErrors {
+        fds: Vec<PathBuf>,
+    },
+    OutputApproximationMetrics {
+        gold_fd_path: PathBuf,
+        fds: Vec<PathBuf>,
+    },
+}
+
+impl Command {
+    pub fn fds(&self) -> &[PathBuf] {
+        match self {
+            Self::ErrorSummary { fds }
+            | Self::OutputErrors { fds }
+            | Self::OutputApproximationMetrics { fds, .. } => fds,
+        }
+    }
 }
--- analyze_fds/error-measures/src/main.rs
+++ analyze_fds/error-measures/src/main.rs
@@ -1,4 +1,8 @@
-use std::{collections::HashMap, fs, io};
+use std::{
+    collections::{HashMap, HashSet},
+    fs, io,
+    path::Path,
+};
 
 use clap::Parser;
 use polars::prelude::*;
@@ -10,18 +14,18 @@ mod stripped_partition;
 use cli::{Cli, Command};
 use stripped_partition::StrippedPartition;
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Clone, Deserialize, PartialEq, Eq, Hash)]
 #[serde(rename_all = "camelCase")]
 struct Identifier {
     column_identifier: String,
 }
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Clone, Deserialize, PartialEq, Eq, Hash)]
 #[serde(rename_all = "camelCase")]
 struct Determinant {
     column_identifiers: Vec<Identifier>,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Clone, Deserialize, PartialEq, Eq, Hash)]
 struct Line {
     determinant: Determinant,
     dependant: Identifier,
@@ -54,47 +58,43 @@ fn main() -> anyhow::Result<()> {
             .clone()
     };
 
-    for fd_path in args.fd_path.iter() {
-        let fd_file = fs::read_to_string(fd_path)?;
-        let fds: Vec<Line> = fd_file
-            .lines()
-            .map(serde_json::from_str)
-            .collect::<Result<_, _>>()?;
+    let mut error_of = |fd: &Line| {
+        let mut error = 0;
+        let x_part = stripped_partition_of(
+            fd.determinant
+                .column_identifiers
+                .iter()
+                .map(|ident| ident.column_identifier.clone())
+                .collect::<Vec<_>>(),
+        );
+        let a_part = stripped_partition_of(vec![fd.dependant.column_identifier.clone()]);
+        let xa_part = x_part.product_in(&a_part, &full_df);
+        let mut t: HashMap<usize, usize> = HashMap::new();
+        for partition in xa_part.partitions.into_iter() {
+            let &tuple = partition.iter().next().unwrap();
+            t.insert(tuple, partition.len());
+        }
+        for partition in x_part.partitions.into_iter() {
+            error += partition.len();
+            let m = partition
+                .into_iter()
+                .flat_map(|tuple| t.get(&tuple).copied())
+                .max()
+                .unwrap_or(1);
+            error -= m;
+        }
 
-        let error_counts = fds.into_iter().map(|fd| {
-            let mut error = 0;
-            let x_part = stripped_partition_of(
-                fd.determinant
-                    .column_identifiers
-                    .iter()
-                    .map(|ident| ident.column_identifier.clone())
-                    .collect::<Vec<_>>(),
-            );
-            let a_part = stripped_partition_of(vec![fd.dependant.column_identifier]);
-            let xa_part = x_part.product_in(&a_part, &full_df);
-            let mut t: HashMap<usize, usize> = HashMap::new();
-            for partition in xa_part.partitions.into_iter() {
-                let &tuple = partition.iter().next().unwrap();
-                t.insert(tuple, partition.len());
-            }
-            for partition in x_part.partitions.into_iter() {
-                error += partition.len();
-                let m = partition
-                    .into_iter()
-                    .flat_map(|tuple| t.get(&tuple).copied())
-                    .max()
-                    .unwrap_or(1);
-                error -= m;
-            }
+        error
+    };
 
-            error
-        });
+    for fd_path in args.command.fds().iter() {
+        let fds = read_fds_from_file(fd_path)?;
+        let error_counts = fds.iter().map(&mut error_of);
 
-        let errors = error_counts.map(|error| error as f64 / full_df.height() as f64);
-        let errors = Series::new("error", errors.collect::<Vec<_>>());
-
-        match args.command {
-            Command::ErrorSummary => {
+        match &args.command {
+            Command::ErrorSummary { .. } => {
+                let errors = error_counts.map(|error| error as f64 / full_df.height() as f64);
+                let errors = Series::new("error", errors.collect::<Vec<_>>());
                 let median_error = errors.median().unwrap();
                 let error_df = DataFrame::new(vec![errors])?;
                 let stats = error_df.describe(None);
@@ -102,15 +102,40 @@ fn main() -> anyhow::Result<()> {
                     stats.vstack(&df! { "describe" => ["median"], "error" => [median_error]}?)?;
                 println!("{}", stats);
             }
-            Command::OutputErrors => {
+            Command::OutputErrors { .. } => {
+                let errors = error_counts.map(|error| error as f64 / full_df.height() as f64);
+                let errors = Series::new("error", errors.collect::<Vec<_>>());
                 let mut error_df = df! {
                     fd_path.file_name().unwrap().to_str().unwrap() => [errors],
                 }
                 .unwrap();
                 JsonWriter::new(io::stdout()).finish(&mut error_df)?;
             }
+            Command::OutputApproximationMetrics { gold_fd_path, .. } => {
+                let gold = read_fds_from_file(&gold_fd_path)?;
+
+                let gold_set: HashSet<Line> = HashSet::from_iter(gold.iter().cloned());
+                let discovered_gold_fd_count =
+                    fds.iter().filter(|fd| gold_set.contains(fd)).count();
+                let completeness = discovered_gold_fd_count as f64 / gold.len() as f64;
+
+                let correct_fd_count = error_counts.filter(|&err| err == 0).count();
+                let correctness = correct_fd_count as f64 / fds.len() as f64;
+
+                println!("File: {}", fd_path.display());
+                println!("  Completeness: {}", completeness);
+                println!("  Correctness:  {}", correctness);
+            }
         }
     }
 
     Ok(())
+}
+
+fn read_fds_from_file(path: &Path) -> anyhow::Result<Vec<Line>> {
+    let fd_file = fs::read_to_string(path)?;
+    Ok(fd_file
+        .lines()
+        .map(serde_json::from_str)
+        .collect::<Result<_, _>>()?)
 }
